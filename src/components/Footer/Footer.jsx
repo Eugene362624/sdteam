8a4f75842ae4a8d853beb18b1fdccc9904eb0932
Eugene Medvedev <eugenemedvedevbelarus@gmail.com>
2021-08-08 00:22:06 +0300

    v0.0.2

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,29 +5,33 @@ import './Footer.scss'
 function Footer() {
     return (
         <footer>
-            <div className="footer-up">
-                <h2>Появилось желание улучшить Ваш бизнес?<br></br>Мы поможем Вам с этим</h2>
-                <button>Запустить проект с нами</button>
-            </div>
-            <div className="">
-                <div className="footer-middle">
-                    <div className="contact-us">
-                        <h2>Свяжитесь с нами</h2>
-                        <a href="">gmail.gmail@gmail.com</a>
+            <div className="footer-left">
+                <div className="footer-up">
+                    <h2>Появилось желание улучшить Ваш бизнес?<br></br>Мы поможем Вам с этим</h2>
+                    <button>Запустить проект с нами</button>
+                </div>
+                <div className="">
+                    <div className="footer-middle">
+                        <div className="contact-us">
+                            <h2>Свяжитесь с нами</h2>
+                            <a href="">gmail.gmail@gmail.com</a>
+                        </div>
+                        <div className="follow-us">
+                            <h2>Следите за нами</h2>
+                            <a href=""><img src='./vk.svg'></img></a>
+                            <a href=""><img src='./instagram.svg'></img></a>
+                            <a href=""><img src='./youtube.svg'></img></a>
+                        </div>
                     </div>
-                    <div className="follow-us">
-                        <h2>Следите за нами</h2>
-                        <a href=""><img src='./vk.svg'></img></a>
-                        <a href=""><img src='./instagram.svg'></img></a>
-                        <a href=""><img src='./youtube.svg'></img></a>
+                    <div className="footer-down">
+                        <img src='./bottomLogo.svg'></img>
+                        <span>2021 &copy; SDTeam Студия веб-разработки. Все права защищены.</span>
                     </div>
                 </div>
-                <div className="footer-down">
-                    <img src='./bottomLogo.svg'></img>
-                    <span>&copy; 2021 - SDTeam студия веб-разработки. Все права защищены.</span>
-                </div>
             </div>
-            
+            <div className="footer-right">
+                <img id="dog" src="./Saly-18.png" alt="" />
+            </div>
         </footer>
     )
 }
